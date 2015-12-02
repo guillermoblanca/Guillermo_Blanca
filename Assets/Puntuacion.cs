65032b224544b82145b585a18cb4429be61d7bec
Guille <staticbangs@gmail.com>
2015-12-02 20:10:38 +0100

    [8] puntuacion
    mediante interfaz introduce puntos si mata a enemigos

---
+++ Assets/Puntuacion.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Puntuacion : MonoBehaviour {
+	public Text textoPuntuacion;
+	public int puntos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textoPuntuacion.text = ""+puntos;
+	}
+	public void Suma_Puntos(){
+		puntos= puntos+ 1;
+	}
+
+}
--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -5,19 +5,28 @@ public class Enemigo : MonoBehaviour {
 	Rigidbody2D rg;
 	public float velocidadEnemigo=1f;
 	public GameObject animacion_explosion;
+	bool muerto;
+	Puntuacion pt;
 	// Use this for initialization
 	void Start () {
+		muerto= false;
 		rg = GetComponent<Rigidbody2D>();
+		pt = GameObject.FindWithTag ("Puntuacion").GetComponent<Puntuacion>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		rg.velocity = new Vector2 (0, -velocidadEnemigo);
+		if (muerto){
+			pt.Suma_Puntos();
+			muerto = false;
+		}
 	}
 	void OnTriggerEnter2D(Collider2D objeto){
 		if (objeto.transform.tag == "Laser"){
 			GameObject animacion = (GameObject) Instantiate(animacion_explosion, transform.position, transform.rotation);
 			velocidadEnemigo = 0;
+			muerto = true;
 			Destroy(gameObject,0.5f);
 			Destroy(animacion,0.5f);
 		
