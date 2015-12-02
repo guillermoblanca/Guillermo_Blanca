8ad6d653142be6939e87f5c114183e6e3ab20b21
Guille <staticbangs@gmail.com>
2015-12-02 18:32:38 +0100

    [1] Movimiento Jugador
    la nave del jugador se mueve de izquierda a derecha hasta los límites de
    la cámara
    Limites creados con BoxCollider
    teclas:
    A: Movimiento Izquierda
    D: Movimiento Derecha

--- Assets/Scripts/Movimiento_CH.cs
+++ Assets/Scripts/Movimiento_CH.cs
@@ -2,14 +2,32 @@
 using System.Collections;
 
 public class Movimiento_CH : MonoBehaviour {
-
+	public float  velocidad=1f;
+	Rigidbody2D rg;
 	// Use this for initialization
 	void Start () {
-	
+		rg = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKey(KeyCode.A)){
+			Mover_Izq(velocidad);
+		}
+		if (Input.GetKeyUp(KeyCode.A)){
+			Mover_Izq(0);
+		}
+		if (Input.GetKey(KeyCode.D)){
+			Mover_Drch(velocidad);
+		}
+		if (Input.GetKeyUp(KeyCode.D)){
+			Mover_Drch(0);
+		}
+	}
+	void Mover_Izq(float velocidad){
+		rg.velocity  = new Vector2 (-velocidad, rg.velocity.y);
+	}
+	void Mover_Drch(float velocidad){
+		rg.velocity  = new Vector2 (velocidad, rg.velocity.y);
 	}
 }
